010b800a8877facfc5fdf08491543483ca4fe225
DiegOps <didevlab@gmail.com>
2024-12-29 17:35:13 +0000

    feat: :triangular_flag_on_post: add validate post mp3 in minio

--- app/main.py
+++ app/main.py
@@ -4,9 +4,7 @@ import shutil
 from minio import Minio
 from minio.error import S3Error
 from utils import green, logger
-import numpy as np
 import moviepy.editor as mp
-from moviepy.editor import AudioFileClip, VideoClip
 from datetime import datetime
 
 current_datetime = datetime.now()
@@ -31,20 +29,6 @@ def verificar_extensao_arquivo_mp3(caminho_arquivo):
     _, extensao = os.path.splitext(caminho_arquivo)
     return extensao.lower() == ".mp3"
 
-# Converte MP3 para MP4
-def converter_mp3_para_mp4(caminho_origem_mp3, caminho_destino_mp4, fps=25):
-    # Carrega o arquivo de áudio MP3
-    audio_clip = AudioFileClip(caminho_origem_mp3)
-
-    # Cria um vídeo em branco com a mesma duração do áudio
-    frame = np.zeros((1920, 1080, 3), dtype=np.uint8) + 255  # imagem branca
-    video_clip = VideoClip(lambda t: frame, duration=audio_clip.duration)
-    video_clip = video_clip.set_fps(fps).set_audio(audio_clip)
-
-    # Salva o arquivo MP4 resultante
-    video_clip.write_videofile(caminho_destino_mp4, codec='libx264', audio_codec='aac', temp_audiofile='temp-audio.m4a', remove_temp=True)
-    logger.debug(green(f"Arquivo convertido para {caminho_destino_mp4}"))
-
 # Cria um diretório, caso ele não exista
 def create_directory(path):
     try:
@@ -53,7 +37,7 @@ def create_directory(path):
     except FileExistsError:
         logger.debug(green(f"Diretório {path} já existe."))
 
-# Função para fazer upload de arquivos para o bucket
+# Faz upload de arquivos para o bucket
 def postFileInBucket(client, bucket_name, path_dest, path_src, content_type=None):
     if path_src.endswith('.txt'):
         content_type = 'text/plain'
@@ -80,11 +64,6 @@ def download_mp3_from_bucket(client, bucket_name):
 
 # Processa o arquivo de áudio e vídeo (edição, conversão e upload para o bucket)
 def process_audio_video(nameProcessedFile, client, bucketSet):
-    nameProcessedFileMp4 = nameProcessedFile[:-4] + ".mp4"
-
-    # Converte MP3 para MP4
-    converter_mp3_para_mp4(f"/app/foredit/{nameProcessedFile}", f"/app/foredit/{nameProcessedFileMp4}")
-
     # Cria diretório para arquivos editados
     pathDirFilesEdited = "/app/edited/"
     create_directory(pathDirFilesEdited)
@@ -93,28 +72,26 @@ def process_audio_video(nameProcessedFile, client, bucketSet):
     margin = os.getenv("AUTO_EDITOR_MARGIN", "0.04sec")
     subprocess.run([
         "auto-editor", 
-        f"./foredit/{nameProcessedFileMp4}",
+        f"./foredit/{nameProcessedFile}",
         "--margin", margin,
-        "-o", f"{pathDirFilesEdited}WithoutSilence-{nameProcessedFileMp4}"
+        "-o", f"{pathDirFilesEdited}WithoutSilence-{nameProcessedFile}"
     ])
     logger.debug(green(f"Editado: {nameProcessedFile}"))
 
     # Reconverte MP4 para MP3
-    clip = mp.AudioFileClip(f"{pathDirFilesEdited}WithoutSilence-{nameProcessedFileMp4}")
+    clip = mp.AudioFileClip(f"{pathDirFilesEdited}WithoutSilence-{nameProcessedFile}")
     clip.write_audiofile(f"{pathDirFilesEdited}{nameProcessedFile}")
 
     # Faz upload do arquivo processado para o bucket
     postFileInBucket(client, bucketSet, f"files-without-silence/{nameProcessedFile}", f"{pathDirFilesEdited}{nameProcessedFile}", 'audio/mpeg')
-    # postFileInBucket(client, bucketSet, f"processing/step1/{currentAction}/Original-{nameProcessedFile}", f"/app/foredit/{nameProcessedFile}", 'audio/mpeg')
 
-    # Remove diretórios temporários
+    # Remove os diretórios temporários usados para edição
     shutil.rmtree(pathDirFilesEdited)
     shutil.rmtree("/app/foredit/")
-    shutil.rmtree("/app/edited/")
 
-    # Remove o arquivo original do bucket
+    # Remove o arquivo original do bucket após o processamento
     client.remove_object(bucketSet, f"{nameProcessedFile}")
-    logger.debug(green(f"Vídeo removido do bucket: {nameProcessedFile}"))
+    logger.debug(green(f"Arquivo original removido do bucket: {nameProcessedFile}"))
 
 # Função principal
 def main():
